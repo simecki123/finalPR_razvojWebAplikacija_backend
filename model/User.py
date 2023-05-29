3f19ad8f21a6ce2e7971f7be5a9341c194e463ce
Sime Roncevic <sroncevic19@gmail.com>
2023-05-29 17:51:40 +0200

    Samo user

--- model/User.py
+++ model/User.py
@@ -2,9 +2,10 @@ from pydantic import BaseModel, Field
 from typing import Optional
 
 class User(BaseModel):
-    id: int
+    id: int = Field(alias="_id")
     name: str
     surname: str
     mail: str
     phone: int
+    role: str
 
--- model/User_car.py
+++ model/User_car.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel, Field
 from typing import Optional
 
 class User_car(BaseModel):
-    id: int
+    id: int = Field(alias="_id")
     id_car: int
     id_user: int
+    
